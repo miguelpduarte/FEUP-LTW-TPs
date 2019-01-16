bcae4f2b0ca470ad2ccec41f7eb29e4bf0ab1f4e
miguelpduarte <miguelpduarte98@gmail.com>
2019-01-16 16:02:00 +0000

    Added solutions for 2015's fase2 ex3 and ex4

---
+++ Exames/2015/fase2/part2 ex3.js
@@ -0,0 +1,41 @@
+// a
+
+const guess_input = document.querySelector("input[name=guess]");
+
+document.querySelector("#guess")
+    .addEventListener("click", () => {
+        if (guess_input.value < secret) {
+            alert("go up");
+            tries++;
+        } else if (guess_input.value > secret) {
+            alert("go down");
+            tries++;
+        } else {
+            correct();
+        }
+    });
+
+// b
+
+const username_input = document.querySelector("input[name=username]");
+
+const correct = () => {
+    alert("correct");
+
+    const xhr = new XMLHttpRequest();
+    // Checking the load event for success and the error event for non-success
+    // An idea also occurred related to checking the response status but I believe
+    //this to be more correct due to the statement mentioning "the script" and
+    //not "the request" or "the server response"
+    xhr.addEventListener("load", () => {
+        alert("script successful");
+    });
+    xhr.addEventListener("error", () => {
+        alert("script unsuccessful");
+    });
+    xhr.open("post", "save_score.php", true);
+    xhr.send(JSON.stringify({
+        username: username_input.value,
+        tries,
+    }));
+};
---
+++ Exames/2015/fase2/part2 ex4.md
@@ -0,0 +1,1 @@
+The solutions for XSD exercises can be found [here.](../../../Other/Secret/wow/yuhere/solution.md)
